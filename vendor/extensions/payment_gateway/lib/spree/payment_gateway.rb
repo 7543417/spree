f9d9bf8eb08eda5c556c5513edbe19d620bf0cb9
Francois Harbec <fharbec@gmail.com>
2011-09-10 14:36:49 -0700

    allow more detailed ActiveMerchant::ConnectionError messages to bubble through

--- vendor/extensions/payment_gateway/lib/spree/payment_gateway.rb
+++ vendor/extensions/payment_gateway/lib/spree/payment_gateway.rb
@@ -19,7 +19,7 @@ module Spree
         :avs_response => response.avs_result['code']
       )
     rescue ActiveMerchant::ConnectionError => e
-      gateway_error I18n.t(:unable_to_connect_to_gateway)
+      gateway_error(e)
     end
 
     def capture(payment)
@@ -43,7 +43,7 @@ module Spree
       )
       payment.finalize!
     rescue ActiveMerchant::ConnectionError => e
-      gateway_error I18n.t(:unable_to_connect_to_gateway)
+      gateway_error(e)
     end
 
     def purchase(amount, payment)
@@ -61,7 +61,7 @@ module Spree
         :avs_response => response.avs_result['code']
       )
     rescue ActiveMerchant::ConnectionError => e
-      gateway_error I18n.t(:unable_to_connect_to_gateway)
+      gateway_error(e)
     end
 
     def void(payment)
@@ -104,7 +104,7 @@ module Spree
       payment.update_attribute(:amount, payment.amount - amount)
       payment.order.update_totals!
     rescue ActiveMerchant::ConnectionError => e
-      gateway_error I18n.t(:unable_to_connect_to_gateway)
+      gateway_error(e)
     end
 
     # find the transaction associated with the original authorization/capture
